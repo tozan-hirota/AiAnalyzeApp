c6ca4f370c5a00efa03d34450ae18692efb545a6
y_hirota_git <y_hirota@tozan.co.jp>
2022-12-01 14:48:58 +0900

    プロジェクト ファイルを追加します。

---
+++ AiAnalyzeApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AiAnalyzeApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AiAnalyzeApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AiAnalyzeApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
